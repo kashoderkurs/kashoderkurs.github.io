ecdbe0d7c6f75a47e9572b603f3ae1b7c58fa804
yannickoo <drupal@yannickoo.de>
2014-05-27 19:44:50 +0200

    Revert "Adds offline functionality"
    This reverts commit 5bf4ca935b4d07b13f15a11e43af84195cc7d693.

--- js/app.js
+++ js/app.js
@@ -1,181 +1,126 @@
 document.addEventListener('DOMContentLoaded', function() {
   "use strict";
 
-  var $ = document.querySelector.bind(document);
-  var $$ = document.querySelectorAll.bind(document);
+  var date = new Date();
+  var hours = date.getHours();
+  var cash = document.querySelector('#cash');
+  var rate = document.querySelector('#rate');
+  var unit = document.querySelector('#unit');
+  var inputs = [cash, rate, unit];
+  var tenner = document.querySelector('#tenner');
+  var rateRadios = document.querySelector('#radios-rate');
+  var unitRadios = document.querySelector('#radios-unit');
+  var share = document.querySelector('#share');
 
-  var kashoderkurs = {
+  if (hours > 21 || hours < 7) {
+    document.body.classList.add('night');
+  }
 
-    hours: new Date().getHours(),
-    cash: $('#cash'),
-    rate: $('#rate'),
-    unit: $('#unit'),
-    input: [cash, rate, unit],
-    tenner: $('#tenner'),
-    rateRadios: $('#radios-rate'),
-    unitRadios: $('#radios-unit'),
-    share: $('#share'),
+  if (location.hash) {
+    var hash = location.hash.substr(1);
+    var hashValues = hash.split('/');
 
-    run: function() {
-      this.changeStyle();
-      this.hash.handle();
-      this.inputs.init();
-    },
+    if (parseInt(hashValues[2]) == hashValues[2]) {
+      hashValues[2] = parseFloat(hashValues[2]).toFixed(2);
+    }
 
-    changeStyle: function() {
-      if (this.hours > 21 || this.hours < 7) {
-        document.body.classList.add('night');
-      }
-    },
-
-    hash: {
-      handle: function() {
-        if (location.hash) {
-          var hash = location.hash.substr(1);
-          var hashValues = hash.split('/');
-
-          if (parseInt(hashValues[2]) == hashValues[2]) {
-            hashValues[2] = parseFloat(hashValues[2]).toFixed(2);
-          }
-
-          if (hashValues.length == 3 && (hashValues[0] / hashValues[1]).toFixed(2) == hashValues[2]) {
-            for (var i = 0, j = inputs.length; i < j; i++) {
-              if (hashValues[i].match(/.00/)) {
-                hashValues[i] = parseInt(hashValues[i]);
-              }
-
-              inputs[i].value = hashValues[i];
-            }
-          }
-          else {
-            for (var i = 0, j = inputs.length; i < j; i++) {
-              inputs[i].value = '';
-            }
-            if (!!(window.history && history.pushState)) {
-              history.pushState(document.title, '', window.location.protocol + '//' + location.host + window.location.pathname + window.location.search);
-            }
-          }
-        }
-      },
-
-      update: function() {
-        var hash = [];
-        for (var i = 0, j = inputs.length; i < j; i++) {
-          hash[i] = inputs[i].value;
+    if (hashValues.length == 3 && (hashValues[0] / hashValues[1]).toFixed(2) == hashValues[2]) {
+      for (var i = 0, j = inputs.length; i < j; i++) {
+        if (hashValues[i].match(/.00/)) {
+          hashValues[i] = parseInt(hashValues[i]);
         }
 
-        location.hash = hash.join('/');
-      }
-    },
-
-    inputs: {
-      init: function() {
-        console.log(kashoderkurs.inputs);
-        Array.prototype.forEach.call(kashoderkurs.inputs, function(index, value) {
-          console.log(index, value);
-        });
-      },
-      cash: {
-        init: function() {
-          kashoderkurs.cash.addEventListener('input', this.update());
-        },
-        update: function() {
-          var value = this.cash.value;
-          var rateValue = kashoderkurs.rate.value;
-          var unitValue = kashoderkurs.unit.value;
-
-          if (!parseInt(rateValue)) {
-            unit.value = 0;
-            return;
-          }
-
-          var newUnitValue = (value / rateValue).toFixed(2);
-          if (newUnitValue.match(/.00/)) {
-            newUnitValue = parseInt(newUnitValue);
-          }
-
-          kashoderkurs.unit.value = newUnitValue;
-        }
-      },
-      rate: {
-        init: function() {
-          kashoderkurs.rate.addEventListener('input', this.update());
-        },
-        update: function() {
-          var value = this.value;
-          var cashValue = kashoderkurs.cash.value;
-          var unitValue = kashoderkurs.unit.value;
-
-          var radio = rateRadios.querySelector('input:checked').value;
-
-          if (radio == 'cash') {
-            var newCashValue = (value * unitValue).toFixed(2);
-            if (newCashValue.match(/.00/)) {
-              newCashValue = parseInt(newCashValue);
-            }
-
-            cash.value = newCashValue;
-          }
-          else if (radio == 'unit') {
-            if (!parseInt(value)) {
-              unit.value = 0;
-              return;
-            }
-
-            var newUnitValue = (cashValue / value).toFixed(2);
-            if (newUnitValue.match(/.00/)) {
-              newUnitValue = parseInt(newUnitValue);
-            }
-
-            unit.value = newUnitValue;
-          }
-        }
-      },
-      unit: {
-        init: function() {
-          unit.addEventListener('input', this.update());
-        },
-        update: function() {
-          var value = this.value;
-          var rateValue = rate.value;
-          var cashValue = cash.value;
-
-          var radio = unitRadios.querySelector('input:checked').value;
-
-          if (radio == 'cash') {
-            var newCashValue = (value * rateValue).toFixed(2);
-            if (newCashValue.match(/.00/)) {
-              newCashValue = parseInt(newCashValue);
-            }
-
-            cash.value = newCashValue;
-          }
-          else if (radio == 'rate') {
-            if (!value) {
-              rate.value = 0;
-              return;
-            }
-
-            var newRateValue = (cashValue / value).toFixed(2);
-            if (newRateValue.match(/.00/)) {
-              newRateValue = parseInt(newRateValue);
-            }
-
-            rate.value = newRateValue;
-          }
-        }
+        inputs[i].value = hashValues[i];
       }
     }
-  };
+    else {
+      for (var i = 0, j = inputs.length; i < j; i++) {
+        inputs[i].value = '';
+      }
+      if (!!(window.history && history.pushState)) {
+        history.pushState(document.title, '', window.location.protocol + '//' + location.host + window.location.pathname + window.location.search);
+      }
+    }
+  }
 
-  kashoderkurs.run();
+  var cashInput = function() {
+    var value = cash.value;
+    var rateValue = rate.value;
+    var unitValue = unit.value;
+
+    if (!parseInt(rateValue)) {
+      unit.value = 0;
+      return;
+    }
+
+    var newUnitValue = (value / rateValue).toFixed(2);
+    if (newUnitValue.match(/.00/)) {
+      newUnitValue = parseInt(newUnitValue);
+    }
+
+    unit.value = newUnitValue;
+  }
+
+  cash.addEventListener('input', cashInput);
 
   rate.addEventListener('input', function() {
+    var value = this.value;
+    var cashValue = cash.value;
+    var unitValue = unit.value;
 
+    var radio = rateRadios.querySelector('input:checked').value;
+
+    if (radio == 'cash') {
+      var newCashValue = (value * unitValue).toFixed(2);
+      if (newCashValue.match(/.00/)) {
+        newCashValue = parseInt(newCashValue);
+      }
+
+      cash.value = newCashValue;
+    }
+    else if (radio == 'unit') {
+      if (!parseInt(value)) {
+        unit.value = 0;
+        return;
+      }
+
+      var newUnitValue = (cashValue / value).toFixed(2);
+      if (newUnitValue.match(/.00/)) {
+        newUnitValue = parseInt(newUnitValue);
+      }
+
+      unit.value = newUnitValue;
+    }
   });
 
   unit.addEventListener('input', function() {
+    var value = this.value;
+    var rateValue = rate.value;
+    var cashValue = cash.value;
 
+    var radio = unitRadios.querySelector('input:checked').value;
+
+    if (radio == 'cash') {
+      var newCashValue = (value * rateValue).toFixed(2);
+      if (newCashValue.match(/.00/)) {
+        newCashValue = parseInt(newCashValue);
+      }
+
+      cash.value = newCashValue;
+    }
+    else if (radio == 'rate') {
+      if (!value) {
+        rate.value = 0;
+        return;
+      }
+
+      var newRateValue = (cashValue / value).toFixed(2);
+      if (newRateValue.match(/.00/)) {
+        newRateValue = parseInt(newRateValue);
+      }
+
+      rate.value = newRateValue;
+    }
   });
 
   unit.addEventListener('focus', function() {
@@ -208,6 +153,12 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   var updateHash = function() {
+    var hash = [];
+    for (var i = 0, j = inputs.length; i < j; i++) {
+      hash[i] = inputs[i].value;
+    }
+
+    location.hash = hash.join('/');
   };
 
   for (var i = 0, j = inputs.length; i < j; i++) {
@@ -218,7 +169,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.addEventListener('keyup', function(e) {
     if (document.activeElement == rate || document.activeElement == unit) {
-      var radioInputs = $$('#radios-' + document.activeElement.id + ' input');
+      var radioInputs = document.querySelectorAll('#radios-' + document.activeElement.id + ' input');
 
       if (e.keyCode === 37 || e.keyCode === 39) {
         if (e.keyCode === 37) {
@@ -235,7 +186,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   })
 
-  var playableAudio = $$('[data-type="audio"]');
+  var playableAudio = document.querySelectorAll('[data-type="audio"]');
   Array.prototype.forEach.call(playableAudio, function(playableElement) {
     var src = playableElement.getAttribute('data-src');
     var audio = new Audio(src);
